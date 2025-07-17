bfe88ce29e9126b1a95d40bd3bded7ef09c5aaec
Anirudh-Reddy <anirudhreddy531@gmail.com>
2025-07-17 14:02:33 -0400

    cors issue

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ import express from 'express';
 import { connectDB } from './src/config/db.js';
 import teamRoutes from './src/routes/teamRoutes.js';
 import matchRoutes from './src/routes/matchRoutes.js';
+import photoRoutes from './src/routes/photo.routes.js';
+import announcementRoutes from './src/routes/announcementRoutes.js';
+
 import cors from "cors";
 const app = express();
 
-app.use(cors({
-  origin: 'https://avk-tournaments.vercel.app',
-}));
+const PORT = process.env.PORT || 3000;
 
+app.use(cors({
+  origin: ['https://avk-tournaments.vercel.app', 'http://localhost:4200']
+}));
 
 dotenv.config();
 
@@ -20,9 +24,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/teams', teamRoutes);
 app.use('/api/matches', matchRoutes);
+app.use('/api/announcements', announcementRoutes);
+app.use('/api/photos', photoRoutes);
 
 app.get('/', (req, res) => {
   res.send('✅ AVK iVL Tournament API is live on Vercel!');
 });
 
-export default app; // ✅ Required by Vercel
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
